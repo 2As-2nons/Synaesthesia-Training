43bef070233894f14b95db872e5633f7bc9f7d41
2As-2nons <218667661+2As-2nons@users.noreply.github.com>
2025-07-26 16:14:42 +0200

    Update config before pulling

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Synaesthesia-Training/', // VERY IMPORTANT
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
